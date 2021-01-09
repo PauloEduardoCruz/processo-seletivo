1d438eac3564e92401469db13559bdfba03f6544
PauloEduardoCruz <pauloeduardo.crp@gmail.com>
2021-01-08 23:25:24 -0300

    Página finalizada

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Escribo-processo-seletivo
- Este repositório foi criado para realizar o primeiro teste do processo seletivo, para  a vaga de Engenheiro de Software na empresa Escribo Inovação para o Aprendizado.
+# processo-seletivo
+ Este repositório foi criado para realizar o primeiro teste do processo seletivo, para  a vaga de Engenheiro de Software.
--- javaScript/script.js
+++ javaScript/script.js
@@ -1,24 +1,22 @@
+function somatorio() {
+    valor = document.getElementById("valorRecebido").value;
+    if (valor < 4) {
+        return
+    }
 
-function somatorio(valor) {
-    //document.getElementById("icone").src = foto
     var valoresDivisiveis = []
-    
-    const reducer = (acumulador, valorAtual) => acumulador + valorAtual 
+
+    const reducer = (acumulador, valorAtual) => acumulador + valorAtual
     for (let i = 1; i < valor; i++) {
-            if (i % 3 ==0 || i % 5  ==0) {
-                //console.log("O número " + i + " é Divisivel por 3 ou 5")
-                valoresDivisiveis.push(i)
-            }
+        if (i % 3 == 0 || i % 5 == 0) {
+            console.log("O número " + i + " é Divisivel por 3 ou 5")
+            valoresDivisiveis.push(i)
         }
-    //console.log("Resultado: " , valoresDivisiveis)
-    console.log(valoresDivisiveis.reduce(reducer))
-    
-    //var x = soma.reduce(reducer)
-    //console.log("Resultado: " , x)
+    }
+
+    var x = valoresDivisiveis.reduce(reducer)
+    console.log("Resultado: ", x)
+
+    document.getElementById("primeiraTela").innerHTML = "{ " + x + " }";
+    document.getElementById("segundaTela").innerHTML = "{ " + valoresDivisiveis + " }";
 }
-
-
-
-
-somatorio(10)
-
